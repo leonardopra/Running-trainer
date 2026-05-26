36f8f494716cf8e103525a8a0d5c6d51abe7da2c
leonardopra <leonardo.prando09@gmail.com>
2026-05-26 15:48:40 +0200

    ⏺ refactor(di): remove AppContainer, Hilt is now the sole composition root

--- android-app/app/src/main/java/com/runningtrainer/android/app/RunningTrainerApplication.kt
+++ android-app/app/src/main/java/com/runningtrainer/android/app/RunningTrainerApplication.kt
@@ -4,12 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class RunningTrainerApplication : Application() {
-    lateinit var container: AppContainer
-        private set
-
-    override fun onCreate() {
-        super.onCreate()
-        container = AppContainer(this)
-    }
-}
+class RunningTrainerApplication : Application()
